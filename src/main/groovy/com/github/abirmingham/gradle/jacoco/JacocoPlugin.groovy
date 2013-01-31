e8fcb0a82ff2b8b17b54e01e744a038124d454fb
Alex Birmingham <alexx1523@gmail.com>
2013-01-30 16:06:26 -0800

    Now building with a couple passing tests.

--- src/main/groovy/com/github/abirmingham/gradle/jacoco/JacocoPlugin.groovy
+++ src/main/groovy/com/github/abirmingham/gradle/jacoco/JacocoPlugin.groovy
@@ -2,51 +2,61 @@ package com.github.abirmingham.gradle.jacoco
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.artifacts.Configuration
+import org.gradle.api.plugins.ReportingBasePlugin
 
 class JacocoPlugin implements Plugin<Project> {
 
     static String NAME = "jacoco"
 
-    Configuration addConfiguration(Project project, JacocoPluginExtension extension) {
-        def jacocoVersion = extension.version
+    @Override
+    void apply(Project project) {
+        project.plugins.apply(ReportingBasePlugin)
 
-        return project.configurations.add(NAME).with {
-            visible     = false
-            transitive  = true
-            description = "The Jacoco libraries to be used for this project."
+        JacocoPluginExtension extension = createExtension(project)
+//        Configuration configuration     = addConfiguration(project, extension)
 
-            include group: "org.jacoco:org.jacocoagent:$jacocoVersion"
-            include group: "org.jacoco:org.jacocoant:$jacocoVersion"
+        instrumentTaskTestClasses(project, extension)
 
-            // Don't need these things, they're provided by the runtime
-            exclude group: 'ant',                 module: 'ant'
-            exclude group: 'org.apache.ant',      module: 'ant'
-            exclude group: 'org.apache.ant',      module: 'ant-launcher'
-            exclude group: 'org.codehaus.groovy', module: 'groovy'
-            exclude group: 'org.codehaus.groovy', module: 'groovy-all'
-            exclude group: 'org.slf4j',           module: 'slf4j-api'
-            exclude group: 'org.slf4j',           module: 'jcl-over-slf4j'
-            exclude group: 'org.slf4j',           module: 'log4j-over-slf4j'
-            exclude group: 'commons-logging',     module: 'commons-logging'
-            exclude group: 'log4j',               module: 'log4j'
-        }
+        // TBD execute output
     }
+//
+//    Configuration addConfiguration(Project project, JacocoPluginExtension extension) {
+//        def jacocoVersion = extension.version
+//
+//        return project.configurations.add(NAME).with {
+//            visible     = false
+//            transitive  = true
+//            description = "The Jacoco libraries to be used for this project."
+//
+//            // Don't need these things, they're provided by the runtime
+//            exclude group: 'ant',                 module: 'ant'
+//            exclude group: 'org.apache.ant',      module: 'ant'
+//            exclude group: 'org.apache.ant',      module: 'ant-launcher'
+//            exclude group: 'org.codehaus.groovy', module: 'groovy'
+//            exclude group: 'org.codehaus.groovy', module: 'groovy-all'
+//            exclude group: 'org.slf4j',           module: 'slf4j-api'
+//            exclude group: 'org.slf4j',           module: 'jcl-over-slf4j'
+//            exclude group: 'org.slf4j',           module: 'log4j-over-slf4j'
+//            exclude group: 'commons-logging',     module: 'commons-logging'
+//            exclude group: 'log4j',               module: 'log4j'
+//        }
+//    }
 
     JacocoPluginExtension createExtension(Project project) {
-        return project.extensions.create(name, JacocoPluginExtension, project).with {
+        return project.extensions.create(NAME, JacocoPluginExtension).with {
             reportDir = "${project.reporting.baseDir.absolutePath}/jacoco"
             tmpDir    = "${project.buildDir}/tmp/jacoco"
+            return (JacocoPluginExtension) delegate
         }
     }
 
-    void instrumentTaskTestClasses(Project project, Configuration configuration, JacocoPluginExtension extension) {
-        // Ensure test tasks exists
+    void instrumentTaskTestClasses(Project project, JacocoPluginExtension extension) {
+        // Ensure test tasks exist
         project.apply plugin: 'java'
 
         // Set jvmArgs
-        project.task('testClasses').doFirst {
-            ant.taskdef(name: 'jacocoagent', classname: 'org.jacoco.ant.AgentTask', classpath: configuration.asPath)
+        project.getTasks().findByName('test').with {
+            ant.taskdef(name: 'jacocoagent', classname: 'org.jacoco.ant.AgentTask', classpath: project.configurations.getByName('compile').asPath)
 
             ant.jacocoagent(
                     property:  'agentvmparam',
@@ -58,15 +68,5 @@ class JacocoPlugin implements Plugin<Project> {
 
             jvmArgs "${ant.properties.agentvmparam}"
         }
-    }
-
-    @Override
-    void apply(Project project) {
-        JacocoPluginExtension extension = createExtension(project)
-        Configuration configuration     = addConfiguration(project, extension)
-
-        instrumentTaskTestClasses(project, configuration, extension)
-
-        // TBD execute output
     }
 }
--- src/test/groovy/com/github/abirmingham/gradle/jacoco/JacocoPluginTest.groovy
+++ src/test/groovy/com/github/abirmingham/gradle/jacoco/JacocoPluginTest.groovy
@@ -5,9 +5,10 @@ import org.gradle.testfixtures.ProjectBuilder
 import org.junit.Before
 import org.junit.Test
 
+import static junit.framework.TestCase.assertTrue
 import static org.junit.Assert.assertNotNull
 
-public class JacocoPluginTest {
+class JacocoPluginTest {
 
     Project project
 
@@ -18,7 +19,15 @@ public class JacocoPluginTest {
     }
 
     @Test
-    public void shouldAddConfiguration() {
-        assertNotNull project.configurations.getByName("jacoco")
+    public void shouldAddExtension() {
+        assertNotNull(project.extensions.getByName('jacoco'))
+    }
+
+    @Test
+    public void shouldSetJvmArgs() {
+        assertNotNull(project.test)
+
+        assertTrue(project.getTasks().findByName('test').jvmArgs.any { it.matches('.*jacoco\\.exe.*') })
+        // e.g. -javaagent:/var/folders/vv/9vg9d_f975q9s_rl8gbdzbk00000gn/T/jacocoagent8666481667761165084.jar=destfile=/private/var/folders/vv/9vg9d_f975q9s_rl8gbdzbk00000gn/T/gradle40938656179684856projectDir/build/tmp/jacoco/jacoco.exe,append=false,dumponexit=true,output=file
     }
 }
